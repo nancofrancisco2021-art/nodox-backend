1120f1125c18d3a3b0e7ac74edf4c4abb1b1a51c
Francisco Nanco <francisconanco85@gmail.com>
2026-05-02 15:05:30 -0500

    FORZAR NUEVO BACKEND

--- routes/ordenes.js
+++ routes/ordenes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../db");
+
+console.log("🔥 BACKEND NUEVO ACTIVO 🔥");
  
 function calcularConsumoStock(m) {
     const cantidad = Number(m.cantidad || 0);
